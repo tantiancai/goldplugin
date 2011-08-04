2e14d7083c91086929aa3b76d26b2b99f5d7d0ee
tantiancai@gmail.com <tantiancai@gmail.com@69c9a32d-69dd-3eb3-809c-cc8043f65047>
2011-08-04 16:02:29 +0000

    V1.77

--- trunk/GoldPlugin.js
+++ trunk/GoldPlugin.js
@@ -32,7 +32,7 @@ function _GoldPluginInit()
     var agt = navigator.userAgent.toLowerCase();
     var h = '';
     h += '<div id="_GoldPlugin" style="overflow:auto; width: 220px; height: 260px;">';
-    h += ' <form id="_book" onsubmit="return false;">V1.76';
+    h += ' <form id="_book" onsubmit="return false;">V1.77';
     h += '    买入数量：<input id="_txtMount" type="text" size="5" value="100">';
     h += '    <br />';
     h += '    <input id="_btnAutoStart" onclick="_Init();_AutoStart();" type="submit" value="开始">';
