82c794463cb823987465868ded52eb5e29ea5a42
tantiancai@gmail.com <tantiancai@gmail.com@69c9a32d-69dd-3eb3-809c-cc8043f65047>
2011-10-31 03:28:26 +0000

    V1.87 由最高价和谷底的差不大于2.5%改成高位和谷底的差不大于2.5%

--- trunk/GoldPlugin.js
+++ trunk/GoldPlugin.js
@@ -35,7 +35,7 @@ function _GoldPluginInit()
     var agt = navigator.userAgent.toLowerCase();
     var h = '';
     h += '<div id="_GoldPlugin" style="overflow:auto; width: 220px; height: 260px;">';
-    h += ' <form id="_book" onsubmit="return false;">V1.86';
+    h += ' <form id="_book" onsubmit="return false;">V1.87';
     h += '    买入数量：<input id="_txtMount" type="text" size="5" value="100">';
     h += '    <br />';
     h += '    <input id="_btnAutoStart" onclick="_Init();_AutoStart();" type="submit" value="开始">';
@@ -791,8 +791,8 @@ function _AnalyzeData_level1(oilprices)
 				element = _fluctuations.pop();
 				if ( element == -1 )
 				{
-					//谷底和最大值的差在2.5%之内
-					if ( _Round( max - bottom ) < _Round( price * 2.5 / 100 ) )
+					//高位和谷底的差在2.5%之内
+					if ( _Round( _TopPrice - bottom ) < _Round( price * 2.5 / 100 ) )
 					{
 						_level = 3;		//5秒查询当前报价
 						_status = 1;	//买
